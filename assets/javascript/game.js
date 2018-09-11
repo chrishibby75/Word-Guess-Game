b37dd8442af8e545febd322fd416486106d4907d
chrishibby75 <chrishibby75@gmail.com>
2018-09-11 13:30:37 -0600

    word

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -33,3 +33,64 @@ function resetGame() {
     document.getElementById("you-win-image").style.cssText="display: none";
     updateDisplay();
 };
+
+function updateDisplay() {
+    document.getElementById("totalWins").innerText = wins;
+    document.getElementById("currentWord").innerText = "";
+    for (var i = 0; i < guessingWord.length; i++) {
+        document.getElementById("currentWord").innerText += guessingWord[i];
+    }
+    document.getElementById("remainingGuesses").innerText = remainingGuesses;
+    document.getElementById("guessedLetters").innerText = guessedLetters;
+    if(remainingGuesses <= 0) {
+        document.getElementById("game-over-image").style.cssText = "display: block";
+        document.getElementById("pressKeyTryAgain").style.cssText = "display: block";
+        hasFinished = true;
+    }
+};
+document.onkeyup(event) {
+    if(hasFinished) {
+        resetGame();
+        hasFinished = false;
+    } else {
+        if(event.keyCode >= 65 && event.keyCode <= 90) {
+            makeGuess(event.key.toLowerCase());
+        }
+    }
+};
+function makeGuess (letter) {
+    if(remainingGuesses > 0) {
+        if (!gameStarted) {
+            gameStarted = true;
+        }
+        if(guessedLetters.indexOf(letter) === -1) {
+            guessedLetters.push(letter);
+            evaluateGuess(letter);
+        }
+    }
+    updateDisplay();
+    checkWin();
+};
+function evaluateGuess(letter) {
+    var positions = [];
+    for(i = 0; i < selectableWords[currentWordIndex].length; i++) {
+        if(selectableWords[currentWordIndex][i] === letter) {
+            positions.push(i);
+        }
+    }
+    if(positions.length <= 0) {
+        remainingGuesses--;        
+    } else {
+        for(var i=0; i < positions.length; i++) {
+            guessingWord[positions[i]] = letter;
+        }
+    }
+    function checkWin() {
+        if (guessingWord.indexOf("_") === -1) {
+            document.getElementById("you-win-image").style.cssText = "display:block";
+            document.getElementById("pressKeyTryAgain").style.cssText = "display:block";
+            wins++;
+            hasFinished = true;
+        }
+    }
+};
